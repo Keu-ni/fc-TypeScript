d150a86ba0e62f94ade57fe5bf1ddbf27dbf4f48
Keu-ni <skcor8909@gmail.com>
2021-01-22 07:25:53 +0900

    generic 조건부 타입 설정하기

--- src/generic2.ts
+++ src/generic2.ts
@@ -1,7 +1,25 @@
-class LocalDB<T> {
+interface DB<T> {
+    add(v: T): void;
+    get(): T;
+}
+
+class D<T> implements DB<T> {
+    add(v: T): void {
+        throw new Error("Method not implemented.");
+    }
+    get(): T {
+        throw new Error("Method not implemented.");
+    }
+}
+
+interface JSONSerialize {
+    serialize(): string;
+}
+
+class LocalDB<T extends JSONSerialize> {
     constructor(private localStorageKey: string){}
     add(v: T) {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(v));
+        localStorage.setItem(this.localStorageKey, v.serialize());
     }
     get(): T {
         const v = localStorage.getItem(this.localStorageKey);
@@ -9,9 +27,24 @@ class LocalDB<T> {
     }
 }
 
-interface User { name: string }
+interface Vegetable {
+    v: string;
+}
 
-const userDB = new LocalDB<User>('user');
-userDB.add({ name: 'ahn' });
-const userA = userDB.get();
-userA.name;
+interface Meat {
+    m: string;
+}
+
+interface Cart2<T> {
+    getItem(): T extends Vegetable ? Vegetable :  Meat
+}
+
+const cart1: Cart2<Vegetable> = {
+    getItem() {
+        return {
+            v: ''
+        }
+    }
+}
+
+cart1.getItem()
